91fd52808669db2e01f7b3292eb8399f0a4e0866
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 15:02:24 -0400

    Update webserver.js

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -1,4 +1,4 @@
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var http = require('http')//.createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var express = require('express');
 var app = express();
@@ -7,21 +7,21 @@ var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-http.listen(8080); //listen to port 8080
+// http.listen(8080); //listen to port 8080
+//
+// function handler (req, res) { //create server
+//   fs.readFile(__dirname + "/public/index.html", function(err, data) { //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+//     res.write(data); //write data from index.html
+//     return res.end();
+//   });
+// }
 
-function handler (req, res) { //create server
-  fs.readFile(__dirname + "/public/index.html", function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-}
-
-app.use(express.static(__dirname + 'js')); //Serves resources from public folder
+app.use(express.static('public')); //Serves resources from public folder
 
 
 var server = app.listen(8080);
