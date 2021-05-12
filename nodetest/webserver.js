214e677f0cfb1bff011006ebfdf1fc2878269225
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 14:17:36 -0400

    updating dependencies and webserver

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -1,29 +1,41 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
-var fs = require('fs'); //require filesystem module
+var static = require('node-static');
+//var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-http.listen(8080); //listen to port 8080
+// http.listen(8080); //listen to port 8080
+//
+// function handler (req, res) { //create server
+//   fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+//     res.write(data); //write data from index.html
+//     return res.end();
+//   });
+// }
 
-function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    fs.readFile(__dirname + '/public/js/randm.js', function(err, data) { //read file index.html in public folder
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-        return res.end("404 Not Found");
-      }
-    )
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-}
+var fileServer = new static.Server('./public');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response, function (err, result) {
+            if (err) { // There was an error serving the file
+                console.error("Error serving " + request.url + " - " + err.message);
+
+                // Respond to the client
+                response.writeHead(err.status, err.headers);
+                response.end();
+            }
+        });
+    }).resume();
+}).listen(8080);
+
 
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   var lightvalue = 0; //static variable for current status
