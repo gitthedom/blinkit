367131382f0e841a4d5ced6603dec68932862f41
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 14:38:17 -0400

    Update webserver.js

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -8,7 +8,7 @@ var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'bo
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
-  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+  fs.readFile(__dirname + req.url, function(err, data) { //read file index.html in public folder
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
       return res.end("404 Not Found");
@@ -18,6 +18,8 @@ function handler (req, res) { //create server
     return res.end();
   });
 }
+
+app.use('/js', express.static('public'));
 
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   var lightvalue = 0; //static variable for current status
