4024a63c995b0e7554d37c4d1834543d3b6324bd
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 14:44:45 -0400

    Update webserver.js

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -1,23 +1,35 @@
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var http = require('http') //.createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-http.listen(8080); //listen to port 8080
+//http.listen(8080); //listen to port 8080
 
-function handler (req, res) { //create server
-  fs.readFile(__dirname + req.url, function(err, data) { //read file index.html in public folder
+// function handler (req, res) { //create server
+//   fs.readFile(__dirname + req.url, function(err, data) { //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+//     res.write(data); //write data from index.html
+//     return res.end();
+//   });
+// }
+
+http.createServer(function (req, res) {
+  fs.readFile(__dirname + req.url, function (err,data) {
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
     }
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
+    res.writeHead(200);
+    res.end(data);
   });
-}
+}).listen(8080);
 
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   var lightvalue = 0; //static variable for current status
