9c56b552397644697370487382dbfbd84b05f9d8
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 15:17:16 -0400

    Update webserver.js

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -1,7 +1,4 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
-var express = require('express');
-var app = express();
-var server = http.createServer(app);
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
