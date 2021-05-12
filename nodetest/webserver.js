1bc810453524b12464a744da167070334bcaf91e
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 11:21:47 -0400

    https

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -1,11 +1,11 @@
-var http = require('http').createServer(handler); //require http server, and create server with function handler()
+var https = require('https').createServer(handler); //require https server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
-var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var io = require('socket.io')(https) //require socket.io module and pass the https object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-http.listen(8080); //listen to port 8080
+https.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
   fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
@@ -38,7 +38,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
   });
 });
 
-process.on('SIGTERM', function () { //on ctrl+c
+process.on('SIGINT', function () { //on ctrl+c
   LED.writeSync(0); // Turn LED off
   LED.unexport(); // Unexport LED GPIO to free resources
   pushButton.unexport(); // Unexport Button GPIO to free resources
