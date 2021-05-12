72e1b19d694d9fafc7bae3e3c9173917147401b9
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 11:27:00 -0400

    Update webserver.js

--- nodetest/webserver.js
+++ nodetest/webserver.js
@@ -1,11 +1,11 @@
-var https = require('https').createServer(handler); //require https server, and create server with function handler()
+var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
-var io = require('socket.io')(https) //require socket.io module and pass the https object (server)
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-https.listen(8080); //listen to port 8080
+http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //create server
   fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
