2435764135fbe195ae17a1cb0db33f040444e817
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 11:39:17 -0400

    Update randm.js

--- nodetest/randm.js
+++ nodetest/randm.js
@@ -2,6 +2,8 @@ fetch('https://rickandmortyapi.com/api/character/')
    .then(response => response.json())
    .then(characters => showCharacters(characters.results));
 
+console.log('am I here?');
+
  showCharacters = characters => {
    const charactersDiv = document.querySelector('#rick-and-morty-characters');
    characters.forEach(character => {
