0b129263d9191a025688e2bf1fcc429e86e72e10
gitthedom <65032991+gitthedom@users.noreply.github.com>
2021-05-12 13:31:54 -0400

    call function

--- nodetest/public/randm.js
+++ nodetest/public/randm.js
@@ -1,14 +1,16 @@
-fetch('https://rickandmortyapi.com/api/character/')
+function callIt () {
+  fetch('https://rickandmortyapi.com/api/character/')
    .then(response => response.json())
    .then(characters => showCharacters(characters.results));
 
-console.log('am I here?');
+  console.log('am I here?');
 
- var showCharacters = characters => {
-   const charactersDiv = document.querySelector('#rick-and-morty-characters');
-   characters.forEach(character => {
-     const characterElement = document.createElement('p');
-     characterElement.innerText = `Character Name: ${character.name}`;
-     charactersDiv.append(characterElement);
-   });
- };
+  showCharacters = characters => {
+     const charactersDiv = document.querySelector('#rick-and-morty-characters');
+     characters.forEach(character => {
+       const characterElement = document.createElement('p');
+       characterElement.innerText = `Character Name: ${character.name}`;
+       charactersDiv.append(characterElement);
+     });
+   };
+}
